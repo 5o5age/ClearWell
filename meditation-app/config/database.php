3075c2b293783a32526ebdda6cd740681e2f0455
5o5age <gimelotikiti@gmail.com>
2026-05-18 19:09:50 +0300

    Switch production DB driver from SQLite to Postgres for Railway
    - Dockerfile: install libpq-dev + pdo_pgsql instead of libsqlite3-dev + pdo_sqlite
    - Drop the database/ mkdir and touch database.sqlite step
    - .env.production.example: default to pgsql, document DB_URL for Railway's ${{Postgres.DATABASE_URL}}
    - .env.example: keep local on sqlite but document pgsql template

--- meditation-app/config/database.php
+++ meditation-app/config/database.php
@@ -6,28 +6,17 @@ use Pdo\Mysql;
 return [
 
     /*
-    |--------------------------------------------------------------------------
-    | Default Database Connection Name
-    |--------------------------------------------------------------------------
-    |
-    | Here you may specify which of the database connections below you wish
-    | to use as your default connection for database operations. This is
-    | the connection which will be utilized unless another connection
-    | is explicitly specified when you execute a query / statement.
-    |
+    
+     Default Database Connection Name
+   
     */
 
     'default' => env('DB_CONNECTION', 'sqlite'),
 
     /*
-    |--------------------------------------------------------------------------
-    | Database Connections
-    |--------------------------------------------------------------------------
-    |
-    | Below are all of the database connections defined for your application.
-    | An example configuration is provided for each database system which
-    | is supported by Laravel. You're free to add / remove connections.
-    |
+    
+    Database Connections
+  
     */
 
     'connections' => [
@@ -110,21 +99,15 @@ return [
             'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
             'prefix_indexes' => true,
-            // 'encrypt' => env('DB_ENCRYPT', 'yes'),
-            // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
+         
         ],
 
     ],
 
     /*
-    |--------------------------------------------------------------------------
-    | Migration Repository Table
-    |--------------------------------------------------------------------------
-    |
-    | This table keeps track of all the migrations that have already run for
-    | your application. Using this information, we can determine which of
-    | the migrations on disk haven't actually been run on the database.
-    |
+    
+     Migration Repository Table
+ 
     */
 
     'migrations' => [
@@ -133,14 +116,9 @@ return [
     ],
 
     /*
-    |--------------------------------------------------------------------------
-    | Redis Databases
-    |--------------------------------------------------------------------------
-    |
-    | Redis is an open source, fast, and advanced key-value store that also
-    | provides a richer body of commands than a typical key-value system
-    | such as Memcached. You may define your connection settings here.
-    |
+   
+     Redis Databases
+ 
     */
 
     'redis' => [
